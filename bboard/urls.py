7ae2415c587d7aa8fb1a5b4c36f05dc102aa0059
Hamersly <maksimwanderer96@gmail.com>
2019-06-19 00:43:19 +0500

    This is the end of the first chapter

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index_url'),
+    path('<int:rubric_id>/', by_rubric, name='by_rubric_url'),
+    path('add/', BbCreateView.as_view(), name='add_url')
 ]
